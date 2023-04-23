27f0578e7f6407159de8b3b96eee3ae4344e0dff
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-04-23 16:21:16 +0300

    first commit

---
+++ practice.py
@@ -0,0 +1,22 @@
+#create a list that contain fruits
+fruits=["apple","banana","cherry"]
+print(fruits)
+
+#create a variable and assign a value to it
+x=5
+y="john"
+print(x)
+print(y)
+
+#Display the sum of 5+10 using two variables x and y
+x=4
+y=13
+print(x+y)
+
+#using the lenght method to print the length of string
+x="Hello World"
+print(len(x))
+
+#print the second item in the fruit list
+fruits=["apple","banana","cherry"]
+print(fruits[1])
